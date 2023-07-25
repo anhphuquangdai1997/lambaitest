ccfc39c493915cdced1fa91cd5bed0e1a630b88b
phu quang <47989188+anhphuquangdai1997@users.noreply.github.com>
2023-07-25 19:44:40 +0000

    update

--- src/client/components/TodoList.tsx
+++ src/client/components/TodoList.tsx
@@ -1,80 +1,88 @@
-import { type SVGProps } from 'react';
-import * as Checkbox from '@radix-ui/react-checkbox';
-import { useState } from 'react';
-import { api } from '@/utils/client/api';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
-import * as Tabs from '@radix-ui/react-tabs';
+import { useState, type SVGProps } from 'react'
+
+import * as Checkbox from '@radix-ui/react-checkbox'
+import * as Tabs from '@radix-ui/react-tabs'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
+import { api } from '@/utils/client/api'
 
 export const TodoList = () => {
   const [selectedItemIds, setSelectedItemIds] = useState<string[]>([])
-  const [filterType, setFilterType] = useState<'all' | 'completed' | 'pending'>('all')
-  const [parent, enableAnimations] = useAutoAnimate()
-  const { data: todos = [],refetch  } = api.todo.getAll.useQuery({
+  const [filterType, setFilterType] = useState<'all' | 'completed' | 'pending'>(
+    'all'
+  )
+  const [parent] = useAutoAnimate()
+  const { data: todos = [], refetch } = api.todo.getAll.useQuery({
     statuses: ['completed', 'pending'],
-  });
+  })
 
-  const { mutate: deleteTodo } = api.todo.delete.useMutation();
+  const { mutate: deleteTodo } = api.todo.delete.useMutation()
   const handleDeleteTodo = async (id: string) => {
-    const parsedId = parseInt(id);
-    await deleteTodo({ id: parsedId });
+    const parsedId = parseInt(id)
+    await deleteTodo({ id: parsedId })
 
-    refetch();
-  };
-
+    refetch()
+  }
 
   const handleItemClick = (id: string) => {
     if (selectedItemIds.includes(id)) {
-      setSelectedItemIds(selectedItemIds.filter(itemId => itemId !== id));
+      setSelectedItemIds(selectedItemIds.filter((itemId) => itemId !== id))
     } else {
-      setSelectedItemIds([...selectedItemIds, id]);
+      setSelectedItemIds([...selectedItemIds, id])
     }
-  };
+  }
 
-  const filteredTodos = todos.filter(todo => {
+  const filteredTodos = todos.filter((todo) => {
     if (filterType === 'completed') {
-      return selectedItemIds.includes(todo.id.toString());
+      return selectedItemIds.includes(todo.id.toString())
     } else if (filterType === 'pending') {
-      return !selectedItemIds.includes(todo.id.toString());
+      return !selectedItemIds.includes(todo.id.toString())
     }
-    return true;
-  });
+    return true
+  })
 
   return (
     <>
       <Tabs.Root className="TabsRoot" defaultValue="tab1">
-        <Tabs.List className="flex pb-10 gap-2" aria-label="Manage your account">
-          <Tabs.Trigger className={` TabsTrigger px-6 py-3 rounded-full text-sm ${
-            filterType === 'all' ? 'bg-gray-700 text-white' : 'bg-white'
-          }`}
-          onClick={() => setFilterType('all')} value="tab1">
+        <Tabs.List
+          className="flex gap-2 pb-10"
+          aria-label="Manage your account"
+        >
+          <Tabs.Trigger
+            className={` TabsTrigger rounded-full px-6 py-3 text-sm ${
+              filterType === 'all' ? 'bg-gray-700 text-white' : 'bg-white'
+            }`}
+            onClick={() => setFilterType('all')}
+            value="tab1"
+          >
             All
           </Tabs.Trigger>
-          <Tabs.Trigger className={` TabsTrigger px-6 py-3 rounded-full text-sm ${
-            filterType === 'pending' ? 'bg-gray-700 text-white' : 'bg-white'
-          }`}
-          onClick={() => setFilterType('pending')} value="tab2">
+          <Tabs.Trigger
+            className={` TabsTrigger rounded-full px-6 py-3 text-sm ${
+              filterType === 'pending' ? 'bg-gray-700 text-white' : 'bg-white'
+            }`}
+            onClick={() => setFilterType('pending')}
+            value="tab2"
+          >
             Pending
           </Tabs.Trigger>
-          <Tabs.Trigger className={` TabsTrigger px-6 py-3 rounded-full text-sm ${
-            filterType === 'completed' ? 'bg-gray-700 text-white' : 'bg-white'
-          }`}
-          onClick={() => setFilterType('completed')} value="tab2">
+          <Tabs.Trigger
+            className={` TabsTrigger rounded-full px-6 py-3 text-sm ${
+              filterType === 'completed' ? 'bg-gray-700 text-white' : 'bg-white'
+            }`}
+            onClick={() => setFilterType('completed')}
+            value="tab2"
+          >
             Completed
           </Tabs.Trigger>
         </Tabs.List>
-        
-        <Tabs.Content className="TabsContent" value="tab1">
-          
-        </Tabs.Content>
-        <Tabs.Content className="TabsContent" value="tab2"></Tabs.Content>
-        <Tabs.Content className="TabsContent" value="tab3"></Tabs.Content>
       </Tabs.Root>
-      <ul className="grid grid-cols-1 gap-y-3">
+      <ul ref={parent} className="grid grid-cols-1 gap-y-3">
         {filteredTodos.map((todo) => (
           <li key={todo.id}>
             <div
-              className={`${selectedItemIds.includes(todo.id.toString())
-                ? 'bg-gray-50':''} flex items-center rounded-12 border border-gray-200 px-4 py-3 shadow-sm`} 
+              className={`${
+                selectedItemIds.includes(todo.id.toString()) ? 'bg-gray-50' : ''
+              } flex items-center rounded-12 border border-gray-200 px-4 py-3 shadow-sm`}
               onClick={() => handleItemClick(todo.id.toString())}
             >
               <Checkbox.Root
@@ -94,12 +102,19 @@ export const TodoList = () => {
                 </Checkbox.Indicator>
               </Checkbox.Root>
 
-              <label className={`block pl-3 font-medium ${selectedItemIds.includes(todo.id.toString()) ?'line-through' :''}`} htmlFor={String(todo.id)}>
+              <label
+                className={`block pl-3 font-medium ${
+                  selectedItemIds.includes(todo.id.toString())
+                    ? 'line-through'
+                    : ''
+                }`}
+                htmlFor={String(todo.id)}
+              >
                 {todo.body}
               </label>
               <button
-                className="flex items-center justify-center ml-auto py-1 bg-red-500 text-black text-sm"
-                onClick={() => handleDeleteTodo(todo.id)}
+                className="bg-red-500 text-black ml-auto flex items-center justify-center py-1 text-sm"
+                onClick={() => handleDeleteTodo(todo.id.toString())}
               >
                 <XMarkIcon className="h-5 w-5" />
               </button>
